ef3694b5594fb916a05400a27957b33449493299
malavikasivakumar <malavikasivakumarshetty@gmail.com>
2024-05-14 23:25:01 +0530

    first commit

---
+++ display.js
@@ -0,0 +1,32 @@
+let menuicon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header navbar');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.getAttribute
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a 
+                [href*='' + id + ']').classList.add
+                ('active')
+            })
+        }
+    })
+}
+
+
+
+
+
+
+
+menuicon.onclick = ()  => {
+    menuicon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
